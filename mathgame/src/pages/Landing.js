dba3798845f56fe473880f0a13d603c698d9e1ca
kubilay <kubidik27@gmail.com>
2022-04-05 02:35:37 +0300

    titlevector created

--- mathgame/src/pages/Landing.js
+++ mathgame/src/pages/Landing.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Title from "../components/Title";
 import style from "../styles/landing.module.css";
+import TitleVector from "../constants/TitleVector";
 
 function Landing() {
   return (
     <div className={style.landing}>
       <Title title="Mathematics Game" />
+      <TitleVector width={640} height={10} color="#FF0000"/>
     </div>
   );
 }
