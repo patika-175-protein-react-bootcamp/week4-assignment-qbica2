6ec94acf757dac0fecec8f7871934acab90161eb
kubilay <kubidik27@gmail.com>
2022-04-06 01:01:07 +0300

    buttonvector created

--- mathgame/src/components/Subtitle.js
+++ mathgame/src/components/Subtitle.js
@@ -2,14 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import style from "../styles/subtitle.module.css";
 
-function Subtitle({ subtitle }) {
+function Subtitle({ subtitle, score }) {
   return (
-    <div className={style.subtitle}>{subtitle}</div>
+    <div className={style.subtitle}>{subtitle} {score}</div>
   );
 }
 
 Subtitle.propTypes = {
-  subtitle: PropTypes.string.isRequired
+  subtitle: PropTypes.string.isRequired,
+  score: PropTypes.number,
 };
 
 export default Subtitle;
--- mathgame/src/pages/Landing.js
+++ mathgame/src/pages/Landing.js
@@ -3,15 +3,24 @@ import style from "../styles/landing.module.css";
 import Title from "../components/Title";
 import TitleVector from "../constants/TitleVector";
 import Subtitle from "../components/Subtitle";
+import ButtonVector from "../constants/ButtonVector";
+
 
 function Landing() {
   return (
     <div className={style.landing}>
-      <Title title="Mathematics Game" />
-      <TitleVector width={640} height={10} color="#FF0000"/>
-      <Subtitle subtitle="Total Points:" />
-      <Subtitle subtitle="Total Questions:" />
-      <Subtitle subtitle="Correct Answers:" />
+      <div className={style.container}> 
+        <div className={style.titleGroup}>
+          <Title title="Mathematics Game" />
+          <TitleVector width={640} height={10} color="#FF0000"/>
+        </div>
+        <Subtitle subtitle="Total Points:" score={129}/>
+        <Subtitle subtitle="Total Questions:" score={40}/>
+        <Subtitle subtitle="Correct Answers:" score={32}/>
+        <button>
+          <ButtonVector width= {447} height= {139} text="Start"/>
+        </button>
+      </div>
     </div>
   );
 }
